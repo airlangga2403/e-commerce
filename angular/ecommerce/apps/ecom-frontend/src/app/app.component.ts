b2d22a5d0f965c392e66d15586376941f551903c
airlangga2403 <airlanggapermana96@gmail.com>
2025-02-27 11:38:00 +0700

    commit

---
+++ angular/ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule, NavbarComponent, FooterComponent],
+  selector: 'ecom-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+  ngOnInit():void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'fas';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+}
---
+++ angular/ecommerce/apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,15 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import {
+  faCartShopping, faTruckFast,
+  faUser
+} from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+export const fontAwesomeIcons : IconDefinition[] = [
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faTwitter,
+  faFacebook
+];
